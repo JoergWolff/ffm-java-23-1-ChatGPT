e2c87dc79f657a4081742c2eb1af74bdae15aed0
JoergWolff <neuerfisch@wolffclan.de>
2023-09-06 10:55:16 +0200

    Create sout for classes

--- src/main/java/de/wolffclan/Main.java
+++ src/main/java/de/wolffclan/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Testing ChatGPT");
+        System.out.println("-------------");
+        System.out.println(PalindromeChecker.isPalindrome("Hollo"));
+        System.out.println(PalindromeChecker.isPalindrome("Anna"));
+        System.out.println(PrimeNumberGenerator.generatePrimeNumbers(10));
+        System.out.println(StringReversal.reverseString("Abracadabra"));
     }
 }
